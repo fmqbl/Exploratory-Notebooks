704a27685ec5ef771205e04690e15f39fd3ccb9c
Faisal Maqbool <faisalmaqbool94@gmail.com>
2019-08-27 16:40:44 +0500

    HAWB Init

---
+++ C-Sharp/GetHouseData/GetHouseData/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace GetHouseData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox3 = new MetroFramework.Controls.MetroTextBox();
+            this.SuspendLayout();
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(293, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(231, 136);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.PromptText = "Enter the file reference";
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(315, 23);
+            this.metroTextBox1.TabIndex = 1;
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMark = "Enter the file reference";
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(315, 262);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(161, 60);
+            this.metroButton2.TabIndex = 3;
+            this.metroButton2.Text = "Create House";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroTextBox2
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox2.CustomButton.Image = null;
+            this.metroTextBox2.CustomButton.Location = new System.Drawing.Point(293, 1);
+            this.metroTextBox2.CustomButton.Name = "";
+            this.metroTextBox2.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox2.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox2.CustomButton.TabIndex = 1;
+            this.metroTextBox2.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox2.CustomButton.UseSelectable = true;
+            this.metroTextBox2.CustomButton.Visible = false;
+            this.metroTextBox2.Lines = new string[0];
+            this.metroTextBox2.Location = new System.Drawing.Point(231, 184);
+            this.metroTextBox2.MaxLength = 32767;
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.PasswordChar = '\0';
+            this.metroTextBox2.PromptText = "Enter the Agent Name";
+            this.metroTextBox2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox2.SelectedText = "";
+            this.metroTextBox2.SelectionLength = 0;
+            this.metroTextBox2.SelectionStart = 0;
+            this.metroTextBox2.ShortcutsEnabled = true;
+            this.metroTextBox2.Size = new System.Drawing.Size(315, 23);
+            this.metroTextBox2.TabIndex = 7;
+            this.metroTextBox2.UseSelectable = true;
+            this.metroTextBox2.WaterMark = "Enter the Agent Name";
+            this.metroTextBox2.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox2.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroTextBox3
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox3.CustomButton.Image = null;
+            this.metroTextBox3.CustomButton.Location = new System.Drawing.Point(293, 1);
+            this.metroTextBox3.CustomButton.Name = "";
+            this.metroTextBox3.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox3.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox3.CustomButton.TabIndex = 1;
+            this.metroTextBox3.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox3.CustomButton.UseSelectable = true;
+            this.metroTextBox3.CustomButton.Visible = false;
+            this.metroTextBox3.Lines = new string[0];
+            this.metroTextBox3.Location = new System.Drawing.Point(231, 87);
+            this.metroTextBox3.MaxLength = 32767;
+            this.metroTextBox3.Name = "metroTextBox3";
+            this.metroTextBox3.PasswordChar = '\0';
+            this.metroTextBox3.PromptText = "Enter the District Code";
+            this.metroTextBox3.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox3.SelectedText = "";
+            this.metroTextBox3.SelectionLength = 0;
+            this.metroTextBox3.SelectionStart = 0;
+            this.metroTextBox3.ShortcutsEnabled = true;
+            this.metroTextBox3.Size = new System.Drawing.Size(315, 23);
+            this.metroTextBox3.TabIndex = 8;
+            this.metroTextBox3.UseSelectable = true;
+            this.metroTextBox3.WaterMark = "Enter the District Code";
+            this.metroTextBox3.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox3.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroTextBox3);
+            this.Controls.Add(this.metroTextBox2);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroTextBox1);
+            this.Name = "Form1";
+            this.Text = "Generate House";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroTextBox metroTextBox3;
+    }
+}
+
---
+++ C-Sharp/GetHouseData/GetHouseData/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MetroFramework;
+using Excel = Microsoft.Office.Interop.Excel;
+using IBM.Data.DB2;
+
+namespace GetHouseData
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //metroLabel1.Hide();
+        }
+        
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            string ConStr = "Database = " + metroTextBox3.Text.Trim() + ";User ID=inetsoft;Password = etl5boxes; server = "+ metroTextBox3.Text.Trim().ToLower()+"."+ metroTextBox3.Text.Trim().ToLower()+".ei:50000; Max Pool Size=100;Persist security info =False;Pooling = True ";
+            string Query = @"Select 
+                sh.SHPMNT_REF,
+                ship.NAME,
+                ship.ADDR_LINE1,
+                ship.ADDR_LINE2,
+                CONCAT(CONCAT(ship.CITY, ','), ship.COUNTRY),
+                cons.NAME,
+                cons.ADDR_LINE1,
+                cons.ADDR_LINE2,
+                CONCAT(CONCAT(cons.CITY, ','), cons.COUNTRY),
+                hdr.ORIGIN_NAME_AWB,
+                hdr.DESTIN_NAME_AWB,
+                hdr.FLIGHT_TO_AWB_1,
+                hdr.FLIGHT_BY_AWB_1,
+                hdr.CHRG_CODE_FRGHT_1,
+                hdr.CHRG_CODE_OTHER_1,
+                sh.PCS_GRS,
+                CAST(ROUND(sh.WGT_GRS_2 / 10000, 0) AS DECIMAL(18, 1)),
+                CAST(ROUND(sh.WGT_CHRG_2 / 10000, 0) AS DECIMAL(18, 1)),
+                hdr.FLIGHT_NUM_AWB_1,
+                hdr.HNDL_INFO_AWB_1,
+                hdr.HNDL_INFO_AWB_2,
+                SUBSTR(xmlserialize(xmlagg(xmltext(CONCAT( '!',concat(concat(itm.REMARKS_AWB_PFX,' '),itm.REMARKS_AWB)))) as VARCHAR(1024)), 3)
+
+                From
+                EXPORT.SHPMNT_HDR sh
+                inner join HELPDB.CLIENT ship on ship.CLIENT_NO = sh.SHIPPER_ID
+                inner join HELPDB.CLIENT cons on cons.CLIENT_NO = sh.CONSIGN_ID
+                inner join EXPORT.AIR_FRGHT_HDR hdr on hdr.INVOICE_REF = sh.SHPMNT_REF
+                inner join EXPORT.AIR_FRGHT_ITEM itm on itm.CARRIER_REF = hdr.CARRIER_REF
+                where sh.SHPMNT_REF = '" + metroTextBox1.Text.Trim() + @"'
+                Group by
+                ship.NAME,
+                ship.ADDR_LINE1,
+                ship.ADDR_LINE2,
+                ship.CITY,
+                ship.COUNTRY,
+                sh.SHPMNT_REF,
+                cons.NAME,
+                cons.ADDR_LINE1,
+                cons.ADDR_LINE2,
+                cons.city,
+                cons.COUNTRY,
+                CAST(ROUND(sh.WGT_GRS_2 / 10000, 0) AS DECIMAL(18, 1)),
+                CAST(ROUND(sh.WGT_CHRG_2 / 10000, 0) AS DECIMAL(18, 1)),
+                hdr.ORIGIN_NAME_AWB,
+                hdr.DESTIN_NAME_AWB,
+                hdr.FLIGHT_TO_AWB_1,
+                hdr.FLIGHT_BY_AWB_1,
+                hdr.CHRG_CODE_FRGHT_1,
+                hdr.CHRG_CODE_OTHER_1,
+                hdr.FLIGHT_NUM_AWB_1,
+                sh.PCS_GRS,
+                hdr.HNDL_INFO_AWB_1,
+                hdr.HNDL_INFO_AWB_2
+                ";
+
+            using (DB2Connection myconnection = new DB2Connection(ConStr))
+            {
+                myconnection.Open();
+                DB2Command cmd = myconnection.CreateCommand();
+                cmd.CommandText = Query;
+                DB2DataReader rd = cmd.ExecuteReader();
+
+                if (rd.HasRows)
+                {
+                    while (rd.Read())
+                    {
+                        //richTextBox1.Text = richTextBox1.Text + rd.GetValue(0).ToString() + "   " + rd.GetValue(1).ToString();
+
+
+                        Excel.Application app = new Excel.Application();
+                        //string filepath = @"HAWBTemp.xlsx";
+                        //Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+                        Excel.Workbook workbook = app.Workbooks.Open(System.IO.Directory.GetCurrentDirectory() + "/HAWBTemplate.xlsx", ReadOnly: false, Editable: true);
+                        Excel.Worksheet worksheet = workbook.Worksheets.Item[1];
+                        //worksheet.Cells[5, 7].Value = rd.GetValue(1).ToString();
+                        //Shipper Update
+                        worksheet.Range["D1"].Value = rd.GetValue(0).ToString();
+                        worksheet.Range["N1"].Value = rd.GetValue(0).ToString();
+                        worksheet.Range["M51"].Value = rd.GetValue(0).ToString();
+                        worksheet.Range["B3"].Value = rd.GetValue(1).ToString();
+                        worksheet.Range["B4"].Value = rd.GetValue(2).ToString();
+                        worksheet.Range["B5"].Value = rd.GetValue(3).ToString();
+                        worksheet.Range["B6"].Value = rd.GetValue(4).ToString();
+
+                        //Consignee Update
+
+                        worksheet.Range["B8"].Value = rd.GetValue(5).ToString();
+                        worksheet.Range["B9"].Value = rd.GetValue(6).ToString();
+                        worksheet.Range["B10"].Value = rd.GetValue(7).ToString();
+                        worksheet.Range["B11"].Value = rd.GetValue(8).ToString();
+
+                        //Origin-Destination
+                        worksheet.Range["B18"].Value = rd.GetValue(9).ToString();
+                        worksheet.Range["B21"].Value = rd.GetValue(10).ToString();
+
+                        //Dest-First Flight
+                        worksheet.Range["B19"].Value = rd.GetValue(11).ToString();
+                        worksheet.Range["C19"].Value = rd.GetValue(12).ToString();
+
+                        //Prepaid-Collect
+
+                        if (rd.GetValue(13).ToString() == "P")
+                        {
+                            worksheet.Range["K19"].Value = " P";
+                        }
+                        else
+                        {
+                            worksheet.Range["K19"].Value = "     C";
+                        }
+
+                        if (rd.GetValue(14).ToString() == "P")
+                        {
+                            worksheet.Range["L19"].Value = "P";
+                        }
+                        else
+                        {
+                            worksheet.Range["L19"].Value = "    C";
+                        }
+                        
+                        //Weight-Pieces
+                        worksheet.Range["B27"].Value = rd.GetValue(15).ToString();
+                        worksheet.Range["C27"].Value = rd.GetValue(16).ToString();
+                        worksheet.Range["G27"].Value = rd.GetValue(17).ToString();
+                        worksheet.Range["B37"].Value = rd.GetValue(15).ToString();
+                        worksheet.Range["C37"].Value = rd.GetValue(16).ToString();
+                        //Flight
+                        worksheet.Range["F21"].Value = rd.GetValue(18).ToString();
+
+                        //Handling Information
+                        worksheet.Range["C23"].Value = rd.GetValue(19).ToString();
+                        worksheet.Range["C24"].Value = rd.GetValue(20).ToString();
+                        worksheet.Range["O46"].Value = metroTextBox2.Text;
+
+                        //Data
+                        DateTime dt = DateTime.Now;
+                        worksheet.Range["I49"].Value = dt.ToShortDateString();
+
+                        //Description
+
+                        String description = rd.GetValue(21).ToString();
+
+                        List<string> descResult = description.Split('!').ToList();
+
+                        descResult = descResult.Where(s => !string.IsNullOrWhiteSpace(s)).Distinct().ToList();
+
+                        //richTextBox1.Text = richTextBox1.Text + descResult[0] +"///////" + descResult[1]+ "//////" + descResult[2];
+
+                         
+                        worksheet.Range["B28"].Value = descResult[0];
+                        worksheet.Range["B29"].Value = descResult[1];
+                        worksheet.Range["B30"].Value = descResult[2];
+                        worksheet.Range["B31"].Value = descResult[3];
+                        worksheet.Range["B32"].Value = descResult[4];
+                        worksheet.Range["B33"].Value = descResult[5];
+                        worksheet.Range["B34"].Value = descResult[6];
+                        worksheet.Range["B35"].Value = descResult[7];
+
+                        /*String remarks = rd.GetValue(22).ToString();
+                        List<string> remarksResult = remarks.Split('!').ToList();
+                        remarksResult = remarksResult.Where(s => !string.IsNullOrWhiteSpace(s)).Distinct().ToList();
+                        for (int i = remarksResult.Count; i <= 7; i++)
+                        {
+                            remarksResult.Add("");
+                        }
+
+                        MetroMessageBox.Show(this,remarksResult[1]);
+                        MetroMessageBox.Show(this, remarksResult[2]);
+                        MetroMessageBox.Show(this, remarksResult[3]);
+                        MetroMessageBox.Show(this, remarksResult[4]);
+                        MetroMessageBox.Show(this, remarksResult[5]);
+
+                        worksheet.Range["M28"].Value = remarksResult[0];
+                        worksheet.Range["M29"].Value = remarksResult[1];
+                        worksheet.Range["M30"].Value = remarksResult[2];
+                        worksheet.Range["M31"].Value = remarksResult[3];
+                        worksheet.Range["M32"].Value = remarksResult[4];
+                        worksheet.Range["M33"].Value = remarksResult[5];
+                        worksheet.Range["M34"].Value = remarksResult[6];
+                        worksheet.Range["M35"].Value = remarksResult[7];*/
+
+                        workbook.ExportAsFixedFormat(Excel.XlFixedFormatType.xlTypePDF,System.IO.Directory.GetCurrentDirectory() + "/"+ metroTextBox1.Text.Trim() +".pdf");
+                        app.Application.ActiveWorkbook.Save();
+                        workbook.Close(0);
+                        app.Application.Quit();
+                        app.Quit();
+
+                        MetroFramework.MetroMessageBox.Show(this, "House Created!");
+                    }
+
+                }
+
+            }
+        }
+
+    }   
+}
