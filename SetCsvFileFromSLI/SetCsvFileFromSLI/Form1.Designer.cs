816930a3b1bc74f4111cfc4465ca996702a5a949
Faisal Maqbool <faisalmaqbool94@gmail.com>
2019-09-01 00:34:02 +0500

    Init

--- SetCsvFileFromSLI/SetCsvFileFromSLI/Form1.Designer.cs
+++ SetCsvFileFromSLI/SetCsvFileFromSLI/Form1.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
             this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.metroButton1.Location = new System.Drawing.Point(154, 101);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(148, 52);
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "Browse and Create CSV";
+            this.metroButton1.UseSelectable = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroButton1);
             this.Name = "Form1";
             this.Text = "Create CSV for ETMS";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +55,8 @@
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
     }
 }
 
