9a09f23a5a04565b0b277c6f30b3c6bbf75e3926
Faisal Maqbool <faisalmaqbool94@gmail.com>
2019-09-03 17:50:00 +0500

    Init

--- PackingListScript/PackingListScript.py
+++ PackingListScript/PackingListScript.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_excel('PackingListFormat.xlsx', usecols=[0,1,2,3,4,5], header=10)
+
 df.drop(df.tail(2).index,inplace=True)
 
 forCsv = df.groupby(['Item Code'], as_index=False)['Item Code','Number of Pieces','Number of Cartons'].sum()
@@ -13,7 +14,6 @@ forCsv.to_csv('packing.csv',index=False)
 outDf = pd.read_csv('output.csv', names =['Item Code','Number of Pieces','Number of Cartons'], dtype=None, index_col =False)
 
 outDf = outDf.groupby(['Item Code'], as_index=False)['Number of Pieces','Number of Cartons'].sum()
-
 outDf['Item Code'] = outDf['Item Code'].astype(str)
 
 outDf['Number of Pieces'] = outDf['Number of Pieces'].astype(int)
@@ -22,10 +22,13 @@ outDf['Number of Cartons'] = outDf['Number of Cartons'].astype(int)
 
 common = forCsv.merge(outDf,on=['Item Code','Number of Pieces','Number of Cartons'])
 
-print('Uncommon or wrong entries in Packing List are : ' )
-print(forCsv[(~forCsv['Item Code'].isin(common['Item Code']))])
+packingList = forCsv[(~forCsv['Item Code'].isin(common['Item Code']))]
+poitm = outDf[(~outDf['Item Code'].isin(common['Item Code']))]
 
-print('Uncommon or wrong entries in POITM screen are: ')
-print(outDf[(~outDf['Item Code'].isin(common['Item Code']))])
+m = {'left_only': 'PackingList', 'right_only': 'POITM', 'both': 'Both'}
+
+result = packingList.merge(poitm, on=['Item Code'], how='outer', indicator='B')
+result['B'] = result['B'].map(m)
 
 
+result.to_csv('Result.csv', index=False, header=['Item','Pieces in PL','Ctns in PL','Pieces in Poitm','Ctns in Poitm','Availability'])
